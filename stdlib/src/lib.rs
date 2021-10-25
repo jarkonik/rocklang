926447be55a2ac1bf55edc851a04def1d86777a7
Jaroslaw Konik <konikjar@gmail.com>
2021-10-25 17:44:36 +0200

    no need to convert

--- stdlib/src/lib.rs
+++ stdlib/src/lib.rs
@@ -3,12 +3,12 @@ pub extern "C" fn printd(x: f32) -> f32 {
     x
 }
 
-pub extern "C" fn vecnew() -> *const std::ffi::c_void {
+pub extern "C" fn vecnew() -> *mut std::vec::Vec<f64> {
     let vec: Vec<f64> = Vec::new();
-    Box::into_raw(Box::new(vec)) as *mut std::ffi::c_void
+    Box::into_raw(Box::new(vec))
 }
 
-pub extern "C" fn vecset(vec: *const Vec<f64>, idx: f64, value: f64) -> *mut std::ffi::c_void {
+pub extern "C" fn vecset(vec: *const Vec<f64>, idx: f64, value: f64) -> *mut std::vec::Vec<f64> {
     let mut v = unsafe { Box::from_raw(vec as *mut Vec<f64>) };
 
     while v.len() < (idx + 1.0) as usize {
@@ -16,7 +16,7 @@ pub extern "C" fn vecset(vec: *const Vec<f64>, idx: f64, value: f64) -> *mut std
     }
     v[idx as usize] = value;
 
-    Box::into_raw(v) as *mut std::ffi::c_void
+    Box::into_raw(v)
 }
 
 pub extern "C" fn vecget(vec: *const Vec<f64>, idx: f64) -> f64 {
