6ed40ae2c8ef13650af48ce8fef0bfa524fca8e5
Jaroslaw Konik <konikjar@gmail.com>
2021-10-25 17:46:32 +0200

    vecfree api

--- stdlib/src/lib.rs
+++ stdlib/src/lib.rs
@@ -8,8 +8,8 @@ pub extern "C" fn vecnew() -> *mut std::vec::Vec<f64> {
     Box::into_raw(Box::new(vec))
 }
 
-pub extern "C" fn vecset(vec: *const Vec<f64>, idx: f64, value: f64) -> *mut std::vec::Vec<f64> {
-    let mut v = unsafe { Box::from_raw(vec as *mut Vec<f64>) };
+pub extern "C" fn vecset(vec: *mut Vec<f64>, idx: f64, value: f64) -> *mut std::vec::Vec<f64> {
+    let mut v = unsafe { Box::from_raw(vec) };
 
     while v.len() < (idx + 1.0) as usize {
         v.push(0.0);
@@ -19,9 +19,15 @@ pub extern "C" fn vecset(vec: *const Vec<f64>, idx: f64, value: f64) -> *mut std
     Box::into_raw(v)
 }
 
-pub extern "C" fn vecget(vec: *const Vec<f64>, idx: f64) -> f64 {
+pub extern "C" fn vecget(vec: *mut Vec<f64>, idx: f64) -> f64 {
     let v = unsafe { Box::from_raw(vec as *mut Vec<f64>) };
     let val = v[idx as usize];
     Box::into_raw(Box::new(v));
     val
+}
+
+pub extern "C" fn vecfree(vec: *mut Vec<f64>, idx: f64) {
+    unsafe {
+        Box::from_raw(vec);
+    }
 }
