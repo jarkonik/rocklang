abd4990ddd3413adfa227721411858b0ca2fdc2f
Jaroslaw Konik <konikjar@gmail.com>
2021-11-05 09:01:22 +0100

    fmt

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -4,12 +4,12 @@ use std::process::Command;
 
 #[test]
 fn prime_sieve() -> Result<(), Box<dyn std::error::Error>> {
-	let mut cmd = Command::cargo_bin("rocklang")?;
+    let mut cmd = Command::cargo_bin("rocklang")?;
 
-	cmd.arg("examples/sieve.rc");
-	cmd.assert()
-		.success()
-		.stdout(predicate::eq("2.000000\n3.000000\n5.000000\n7.000000\n").normalize());
+    cmd.arg("examples/sieve.rc");
+    cmd.assert()
+        .success()
+        .stdout(predicate::eq("2.000000\n3.000000\n5.000000\n7.000000\n").normalize());
 
-	Ok(())
+    Ok(())
 }
