dbe8ed63fdaaf7bee657f6cad70bcbae25d7f1b1
Jarosław Konik <jarkonik@users.noreply.github.com>
2021-11-05 09:14:24 +0100

    sieve test (#10)
    * sieve test

---
+++ tests/integration_tests.rs
@@ -0,0 +1,16 @@
+use assert_cmd::prelude::*;
+use predicates::prelude::*;
+use std::process::Command;
+
+#[test]
+#[cfg_attr(tarpaulin, ignore)]
+fn prime_sieve() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("rocklang")?;
+
+    cmd.arg("examples/sieve.rc");
+    cmd.assert()
+        .success()
+        .stdout(predicate::eq("2.000000\n3.000000\n5.000000\n7.000000\n").normalize());
+
+    Ok(())
+}
