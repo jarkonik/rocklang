0d84233ba1f96585edaf2db298433da24a8b651c
Jaroslaw Konik <konikjar@gmail.com>
2022-08-02 19:30:45 +0200

    whle

--- src/compiler/while_visitor.rs
+++ src/compiler/while_visitor.rs
@@ -1,9 +1,33 @@
 use crate::visitor::WhileVisitor;
 
-use super::{Compiler, CompilerResult, Value};
+use super::{Compiler, CompilerError, CompilerResult, Value};
+use crate::compiler::LLVMCompiler;
+use crate::visitor::Visitor;
 
 impl WhileVisitor<CompilerResult<Value>> for Compiler {
-    fn visit_while(&mut self, _expr: &crate::expression::While) -> CompilerResult<Value> {
-        todo!()
+    fn visit_while(&mut self, expr: &crate::expression::While) -> CompilerResult<Value> {
+        let predicate = match self.walk(&expr.predicate)? {
+            Value::Bool(b) => b,
+            _ => Err(CompilerError::TypeError)?,
+        };
+
+        let fun = self.builder().get_insert_block().get_parent();
+
+        let loop_block = self.context().append_basic_block(&fun, "loop");
+        let after_loop_block = self.context().append_basic_block(&fun, "afterloop");
+
+        self.builder
+            .build_cond_br(&predicate, &loop_block, &after_loop_block);
+
+        self.builder.position_builder_at_end(&loop_block);
+        for stmt in &expr.body {
+            self.walk(&stmt)?;
+        }
+
+        self.builder
+            .build_cond_br(&predicate, &loop_block, &after_loop_block);
+        self.builder.position_builder_at_end(&after_loop_block);
+
+        Ok(Value::Void)
     }
 }
