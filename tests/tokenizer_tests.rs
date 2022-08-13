f760491b3aa7e46d8f44fa9fa0e12d44a8258b0a
Jaroslaw Konik <konikjar@gmail.com>
2022-08-13 11:18:03 +0200

    fix tests

--- tests/tokenizer_tests.rs
+++ tests/tokenizer_tests.rs
@@ -2,6 +2,12 @@ use rocklang::token::TokenKind;
 use rocklang::tokenizer::TokenizerError;
 use rocklang::tokenizer::{Tokenize, Tokenizer};
 
+macro_rules! assert_token_kind_of {
+    ($expected: expr, $actual: expr) => {
+        assert_eq!($expected, $actual.kind);
+    };
+}
+
 #[test]
 fn it_tokenizes_all_tokens() {
     let mut tokenizer = Tokenizer::new(String::from(
@@ -10,50 +16,50 @@ fn it_tokenizes_all_tokens() {
          / identifier identifier123 23123.321 123 <= >= :",
     ));
     let tokens = tokenizer.tokenize().unwrap();
-    assert_eq!(TokenKind::Less, tokens[0]);
-    assert_eq!(TokenKind::Greater, tokens[1]);
-    assert_eq!(TokenKind::LeftParen, tokens[2]);
-    assert_eq!(TokenKind::RightParen, tokens[3]);
-    assert_eq!(TokenKind::Plus, tokens[4]);
-    assert_eq!(TokenKind::Minus, tokens[5]);
-    assert_eq!(TokenKind::Asterisk, tokens[6]);
-    assert_eq!(TokenKind::Percent, tokens[7]);
-    assert_eq!(TokenKind::String(String::from("test")), tokens[8]);
-    assert_eq!(TokenKind::Exclamation, tokens[9]);
-    assert_eq!(TokenKind::NotEqual, tokens[10]);
-    assert_eq!(TokenKind::Or, tokens[11]);
-    assert_eq!(TokenKind::And, tokens[12]);
-    assert_eq!(TokenKind::Equal, tokens[13]);
-    assert_eq!(TokenKind::DoubleEqual, tokens[14]);
-    assert_eq!(TokenKind::Arrow, tokens[15]);
-    assert_eq!(TokenKind::LCurly, tokens[16]);
-    assert_eq!(TokenKind::RCurly, tokens[17]);
-    assert_eq!(TokenKind::Comma, tokens[18]);
-    assert_eq!(TokenKind::Slash, tokens[19]);
-    assert_eq!(
+    assert_token_kind_of!(TokenKind::Less, tokens[0]);
+    assert_token_kind_of!(TokenKind::Greater, tokens[1]);
+    assert_token_kind_of!(TokenKind::LeftParen, tokens[2]);
+    assert_token_kind_of!(TokenKind::RightParen, tokens[3]);
+    assert_token_kind_of!(TokenKind::Plus, tokens[4]);
+    assert_token_kind_of!(TokenKind::Minus, tokens[5]);
+    assert_token_kind_of!(TokenKind::Asterisk, tokens[6]);
+    assert_token_kind_of!(TokenKind::Percent, tokens[7]);
+    assert_token_kind_of!(TokenKind::String(String::from("test")), tokens[8]);
+    assert_token_kind_of!(TokenKind::Exclamation, tokens[9]);
+    assert_token_kind_of!(TokenKind::NotEqual, tokens[10]);
+    assert_token_kind_of!(TokenKind::Or, tokens[11]);
+    assert_token_kind_of!(TokenKind::And, tokens[12]);
+    assert_token_kind_of!(TokenKind::Equal, tokens[13]);
+    assert_token_kind_of!(TokenKind::DoubleEqual, tokens[14]);
+    assert_token_kind_of!(TokenKind::Arrow, tokens[15]);
+    assert_token_kind_of!(TokenKind::LCurly, tokens[16]);
+    assert_token_kind_of!(TokenKind::RCurly, tokens[17]);
+    assert_token_kind_of!(TokenKind::Comma, tokens[18]);
+    assert_token_kind_of!(TokenKind::Slash, tokens[19]);
+    assert_token_kind_of!(
         TokenKind::Identifier(String::from("identifier")),
         tokens[20]
     );
-    assert_eq!(
+    assert_token_kind_of!(
         TokenKind::Identifier(String::from("identifier123")),
         tokens[21]
     );
-    assert_eq!(TokenKind::Numeric(23123.321), tokens[22]);
-    assert_eq!(TokenKind::Numeric(123.0), tokens[23]);
-    assert_eq!(TokenKind::LessOrEqual, tokens[24]);
-    assert_eq!(TokenKind::GreaterOrEqual, tokens[25]);
-    assert_eq!(TokenKind::Colon, tokens[26]);
-    assert_eq!(TokenKind::Eof, tokens[27]);
+    assert_token_kind_of!(TokenKind::Numeric(23123.321), tokens[22]);
+    assert_token_kind_of!(TokenKind::Numeric(123.0), tokens[23]);
+    assert_token_kind_of!(TokenKind::LessOrEqual, tokens[24]);
+    assert_token_kind_of!(TokenKind::GreaterOrEqual, tokens[25]);
+    assert_token_kind_of!(TokenKind::Colon, tokens[26]);
+    assert_token_kind_of!(TokenKind::Eof, tokens[27]);
     assert_eq!(28, tokens.len());
 }
 
 #[test]
 fn it_returns_error_for_unexpected_character() {
     let mut tokenizer = Tokenizer::new(String::from("~"));
-    assert_eq!(
+    assert!(matches!(
+        tokenizer.tokenize(),
         Err(TokenizerError { chr: '~', line: 1 }),
-        tokenizer.tokenize()
-    );
+    ));
 }
 
 #[test]
